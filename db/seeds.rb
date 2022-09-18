c0c062c65c6c7050f76ebef82fa26106eb57dc65
TecnoSigma <tecnooxossi@gmail.com>
2022-09-17 21:25:38 -0300

    Adding image to application background

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,7 @@ Credit.create(url: 'https://www.flaticon.com/free-icons/long-hair',
 Credit.create(url: 'https://www.flaticon.com/free-icons/surgeon',
               title: 'surgeon icons',
               description: 'Surgeon icons created by Freepik - Flaticon')
+
+Credit.create(url: 'https://www.freepik.com/free-vector/simple-background-with-geometric-elements_17665233.htm#&position=0&from_view=collections',
+              title: 'Image by visnezh on Freepik',
+              description: 'Image by visnezh on Freepik')
